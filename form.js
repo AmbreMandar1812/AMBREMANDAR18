fd2966bf22d28929307dcfc189f9509351b39165
AmbreMandar1812 <ambremandar1812@gmail.com>
2022-10-09 14:42:41 +0530

    Add form.js

---
+++ form.js
@@ -0,0 +1,56 @@
+function validate(){
+
+    if( document.my_form.Name.value === ""){
+    alert("Please enter your name!!")
+    document.my_form.Name.focus();
+    return false;}
+
+    if( document.my_form.email.value === "") {
+        alert( "Please provide your Email!" );
+        document.myForm.EMail.focus() ;
+        return false;
+     }
+
+    if(document.my_form.tagline.value.length > 10){
+        alert("The Message exceeds the limit");
+        document.myForm.tagline.focus() ;
+        return false;
+    }
+
+    if(document.my_form.number.value.length != 10){
+        alert("Please Enter a valid Phone Number");
+        document.my_form.number.focus();
+        return false;
+    }
+}
+
+
+
+function generateReceipt(){
+    let name = document.my_form.Name.value
+    let contactNo = document.my_form.number.value
+    let Address = document.my_form.Address.value
+    let quantity = document.my_form.quantity.value
+    let dod = document.my_form.dod.value
+    let d = new Date();
+
+    var opened = window.open(""); 
+    opened.document.write(`
+    <html>
+     <body>
+       <h2>Your Receipt!!</h2>
+       <div style="border: 2px solid black; width:300px; height: 200px; display:grid; place-items: center;">
+
+       <p>You order is Successfully Placed! <br>
+       Date of Generation:${d}</p>
+       Name: ${name}<br>
+       Contact : ${contactNo}<br>
+       Address: ${Address}<br>
+       Quantity: ${quantity}<br>
+       Date of Delivery: ${dod}<br>
+       Thank You for Shopping!!
+       </div>
+     </body>
+    </html>
+`);
+}
